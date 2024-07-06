8a75d7fce1c27fbae7ac085e186c4e1cc24b39dc
Hearsaytoken <areli703@gmail.com>
2024-07-05 22:28:49 -0400

    Update login.tsx

--- frontend/src/pages/login.tsx
+++ frontend/src/pages/login.tsx
@@ -25,31 +25,18 @@ export default function Login() {
   const router = useRouter();
   const dispatch = useAppDispatch();
   const notify = (type, msg) => toast(msg, { type });
-  const [illustrationImage, setIllustrationImage] = useState({
-    src: undefined,
-    photographer: undefined,
-    photographer_url: undefined,
-  });
-  const [illustrationVideo, setIllustrationVideo] = useState({
-    video_files: [],
-  });
+  const [illustrationImage, setIllustrationImage] = useState({});
+  const [illustrationVideo, setIllustrationVideo] = useState({});
   const [contentType, setContentType] = useState('image');
   const [contentPosition, setContentPosition] = useState('left');
 
-  const {
-    currentUser,
-    isFetching,
-    errorMessage,
-    token,
-    notify: notifyState,
-  } = useAppSelector((state) => state.auth);
-  const [initialValues, setInitialValues] = React.useState({
+  const { currentUser, isFetching, errorMessage, token, notify: notifyState } = useAppSelector(state => state.auth);
+  const [initialValues, setInitialValues] = useState({
     email: 'super_admin@flatlogic.com',
     password: 'password',
     remember: true,
   });
 
-  // Fetch Pexels image/video
   useEffect(() => {
     async function fetchData() {
       const image = await getPexelsImage();
@@ -59,25 +46,25 @@ export default function Login() {
     }
     fetchData();
   }, []);
-  // Fetch user data
+
   useEffect(() => {
     if (token) {
       dispatch(findMe());
     }
   }, [token, dispatch]);
-  // Redirect to dashboard if user is logged in
+
   useEffect(() => {
     if (currentUser?.id) {
       router.push('/dashboard');
     }
   }, [currentUser?.id, router]);
-  // Show error message if there is one
+
   useEffect(() => {
     if (errorMessage) {
       notify('error', errorMessage);
     }
   }, [errorMessage]);
-  // Show notification if there is one
+
   useEffect(() => {
     if (notifyState?.showNotification) {
       notify('success', notifyState?.textNotification);
@@ -85,197 +72,99 @@ export default function Login() {
     }
   }, [notifyState?.showNotification]);
 
-  const handleSubmit = async (value) => {
-    const { remember, ...rest } = value;
+  const handleSubmit = async (values) => {
+    const { remember, ...rest } = values;
     await dispatch(loginUser(rest));
   };
 
   const setLogin = (target) => {
     const email = target?.innerText;
-    setInitialValues((prev) => {
-      return { ...prev, email, password: 'password' };
-    });
+    setInitialValues((prev) => ({ ...prev, email, password: 'password' }));
   };
 
   const imageBlock = (image) => (
     <div
-      className='hidden md:flex flex-col justify-end relative flex-grow-0 flex-shrink-0 w-1/3'
+      className='hidden md:flex flex-col justify-end relative w-1/3'
       style={{
-        backgroundImage: `${
-          image
-            ? `url(${image.src?.original})`
-            : 'linear-gradient(rgba(255, 255, 255, 0.5), rgba(255, 255, 255, 0.5))'
-        }`,
+        backgroundImage: `url(${image?.src?.original || ''})`,
         backgroundSize: 'cover',
-        backgroundPosition: 'left center',
-        backgroundRepeat: 'no-repeat',
+        backgroundPosition: 'center',
       }}
     >
-      <div className='flex justify-center w-full bg-blue-300/20'>
-        <a
-          className='text-[8px]'
-          href={image.photographer_url}
-          target='_blank'
-          rel='noreferrer'
-        >
+      <div className='bg-gradient-to-t from-gray-800 via-transparent to-transparent p-4'>
+        <a className='text-xs text-white' href={image.photographer_url} target='_blank' rel='noreferrer'>
           Photo by {image.photographer} on Pexels
         </a>
       </div>
     </div>
   );
 
-  const videoBlock = (video) => {
-    if (video?.video_files?.length > 0) {
-      return (
-        <div className='hidden md:flex flex-col justify-end relative flex-grow-0 flex-shrink-0 w-1/3'>
-          <video
-            className='absolute top-0 left-0 w-full h-full object-cover'
-            autoPlay
-            loop
-            muted
-          >
-            <source src={video.video_files[0]?.link} type='video/mp4' />
-            Your browser does not support the video tag.
-          </video>
-          <div className='flex justify-center w-full bg-blue-300/20 z-10'>
-            <a
-              className='text-[8px]'
-              href={video.user.url}
-              target='_blank'
-              rel='noreferrer'
-            >
-              Video by {video.user.name} on Pexels
-            </a>
-          </div>
+  const videoBlock = (video) => (
+    video?.video_files?.length > 0 && (
+      <div className='hidden md:flex flex-col justify-end relative w-1/3'>
+        <video className='absolute top-0 left-0 w-full h-full object-cover' autoPlay loop muted>
+          <source src={video.video_files[0]?.link} type='video/mp4' />
+          Your browser does not support the video tag.
+        </video>
+        <div className='bg-gradient-to-t from-gray-800 via-transparent to-transparent p-4 z-10'>
+          <a className='text-xs text-white' href={video.user.url} target='_blank' rel='noreferrer'>
+            Video by {video.user.name} on Pexels
+          </a>
         </div>
-      );
-    }
-  };
+      </div>
+    )
+  );
 
   return (
-    <div
-      style={
-        contentPosition === 'background'
-          ? {
-              backgroundImage: `${
-                illustrationImage
-                  ? `url(${illustrationImage.src?.original})`
-                  : 'linear-gradient(rgba(255, 255, 255, 0.5), rgba(255, 255, 255, 0.5))'
-              }`,
-              backgroundSize: 'cover',
-              backgroundPosition: 'left center',
-              backgroundRepeat: 'no-repeat',
-            }
-          : {}
-      }
-    >
+    <div style={contentPosition === 'background' ? { backgroundImage: `url(${illustrationImage?.src?.original || ''})`, backgroundSize: 'cover', backgroundPosition: 'center' } : {}}>
       <Head>
         <title>{getPageTitle('Login')}</title>
       </Head>
 
       <SectionFullScreen bg='violet'>
-        <div
-          className={`flex ${
-            contentPosition === 'right' ? 'flex-row-reverse' : 'flex-row'
-          } min-h-screen w-full`}
-        >
-          {contentType === 'image' && contentPosition !== 'background'
-            ? imageBlock(illustrationImage)
-            : null}
-          {contentType === 'video' && contentPosition !== 'background'
-            ? videoBlock(illustrationVideo)
-            : null}
-          <div className='flex items-center justify-center flex-col space-y-4 w-full lg:w-full'>
+        <div className={`flex ${contentPosition === 'right' ? 'flex-row-reverse' : 'flex-row'} min-h-screen`}>
+          {contentType === 'image' && contentPosition !== 'background' ? imageBlock(illustrationImage) : null}
+          {contentType === 'video' && contentPosition !== 'background' ? videoBlock(illustrationVideo) : null}
+          <div className='flex items-center justify-center flex-col space-y-4 w-full lg:w-2/3'>
             <CardBox className='w-full md:w-3/5 lg:w-2/3'>
-              <h2 className='text-4xl font-semibold my-4'>social pop</h2>
-              <div className='flex flex-row justify-between'>
+              <h2 className='text-4xl font-semibold my-4'>Social Pop</h2>
+              <div className='flex justify-between'>
                 <div>
                   <p className='mb-2'>
-                    Use{' '}
-                    <code
-                      className={'cursor-pointer text-pavitra-blue'}
-                      onClick={(e) => setLogin(e.target)}
-                    >
-                      super_admin@flatlogic.com
-                    </code>{' '}
-                    to login as Super Admin
+                    Use <code className='cursor-pointer text-blue-500' onClick={(e) => setLogin(e.target)}>super_admin@flatlogic.com</code> to login as Super Admin
                   </p>
-
                   <p className='mb-2'>
-                    Use{' '}
-                    <code
-                      className={'cursor-pointer text-pavitra-blue'}
-                      onClick={(e) => setLogin(e.target)}
-                    >
-                      admin@flatlogic.com
-                    </code>{' '}
-                    to login as Admin
+                    Use <code className='cursor-pointer text-blue-500' onClick={(e) => setLogin(e.target)}>admin@flatlogic.com</code> to login as Admin
                   </p>
                   <p>
-                    Use{' '}
-                    <code
-                      className={'cursor-pointer text-pavitra-blue'}
-                      onClick={(e) => setLogin(e.target)}
-                    >
-                      client@hello.com
-                    </code>{' '}
-                    to login as User
+                    Use <code className='cursor-pointer text-blue-500' onClick={(e) => setLogin(e.target)}>client@hello.com</code> to login as User
                   </p>
                 </div>
-                <div>
-                  <BaseIcon
-                    className='text-blue-500'
-                    w='w-16'
-                    h='h-16'
-                    size={48}
-                    path={mdiInformation}
-                  />
-                </div>
+                <BaseIcon className='text-blue-500' w='w-16' h='h-16' size={48} path={mdiInformation} />
               </div>
             </CardBox>
 
             <CardBox className='w-full md:w-3/5 lg:w-2/3'>
-              <Formik
-                initialValues={initialValues}
-                enableReinitialize
-                onSubmit={(values) => handleSubmit(values)}
-              >
+              <Formik initialValues={initialValues} enableReinitialize onSubmit={handleSubmit}>
                 <Form>
                   <FormField label='Login' help='Please enter your login'>
-                    <Field name='email' />
+                    <Field name='email' type='email' className='w-full px-4 py-2 border rounded-md focus:outline-none focus:ring-2 focus:ring-blue-600' />
                   </FormField>
-
                   <FormField label='Password' help='Please enter your password'>
-                    <Field name='password' type='password' />
+                    <Field name='password' type='password' className='w-full px-4 py-2 border rounded-md focus:outline-none focus:ring-2 focus:ring-blue-600' />
                   </FormField>
-
-                  <div className={'flex justify-between'}>
+                  <div className='flex justify-between items-center'>
                     <FormCheckRadio type='checkbox' label='Remember'>
                       <Field type='checkbox' name='remember' />
                     </FormCheckRadio>
-
-                    <Link className={'text-blue-600'} href={'/forgot'}>
-                      Forgot password?
-                    </Link>
+                    <Link href='/forgot' className='text-blue-600'>Forgot password?</Link>
                   </div>
-
                   <BaseDivider />
-
                   <BaseButtons>
-                    <BaseButton
-                      className={'w-full'}
-                      type='submit'
-                      label={isFetching ? 'Loading...' : 'Login'}
-                      color='info'
-                      disabled={isFetching}
-                    />
+                    <BaseButton type='submit' label={isFetching ? 'Loading...' : 'Login'} color='info' disabled={isFetching} className='w-full' />
                   </BaseButtons>
-                  <br />
-                  <p className={'text-center text-gray-600'}>
-                    Don’t have account yet?{' '}
-                    <Link className={'text-blue-600'} href={'/register'}>
-                      New Account
-                    </Link>
+                  <p className='text-center text-gray-600 mt-4'>
+                    Don’t have an account yet? <Link href='/register' className='text-blue-600'>New Account</Link>
                   </p>
                 </Form>
               </Formik>
